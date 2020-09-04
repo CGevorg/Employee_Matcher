ea140bf2385688e1acce178d421d18e3172980d6
CGevorg <CGevorgHarutyunyan@gmail.com>
2020-09-04 02:13:08 -0700

    Adding profiles to the project and changing application.properties file to application.yml file.

--- src/main/java/com/cgev/matcher/exception/CouldNotParseFileException.java
+++ src/main/java/com/cgev/matcher/exception/CouldNotParseFileException.java
@@ -1,5 +1,7 @@
 package com.cgev.matcher.exception;
 
 public class CouldNotParseFileException extends RuntimeException {
-
+    public CouldNotParseFileException(String extension) {
+        super("Could not parse file with given extension: " + extension);
+    }
 }
